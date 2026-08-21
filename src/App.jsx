e8fc7c0aa3333924db65c1e64ddb857195a8c7fb
Angelo <angelo.b.ribeiro@aluno.senai.br>
2026-08-21 08:45:19 -0300

    Modify: finalização dos comentários no componente "Calculate Winner" para melhor entendimento  do código, permitindo que seja refatorado mais facilmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// separar isso em um novo componente
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -7,7 +8,9 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
+//---------------------------------------------
 
+// esse deve ser separado em um componente novo que trata o tabuleiro do jogo, falando onde pode ser jogado
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
@@ -52,6 +55,9 @@ function Board({ xIsNext, squares, onPlay }) {
   );
 }
 
+//-------------------------------------------------------------------
+
+// componente que trata os jogadores
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
@@ -93,23 +99,51 @@ export default function Game() {
     </div>
   );
 }
+//---------------------------------------------------------------------------------
 
+
+
+// componente para fazer o jogador ganhar
 function calculateWinner(squares) {
+
+  //define as linhas que se completas pelo mesmo jogador, o jogo acaba.
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+    [0, 1, 2], // Linha horizontal do topo
+    [3, 4, 5], // Linha horizontal do meio
+    [6, 7, 8], // Linha horizontal de baixo
+    [0, 3, 6], // Coluna vertical da esquerda
+    [1, 4, 7], // Coluna vertical do meio
+    [2, 5, 8], // Coluna vertical da direita
+    [0, 4, 8], // Diagonal principal
+    [2, 4, 6], // Diagonal secundária
+    ];
+
+  // for que verifica todas as linhas de vitória possiveis
   for (let i = 0; i < lines.length; i++) {
+
+    //aqui eçe desestrutura a o que o for manda, em 3 variaveis, sendo elas o "a","b" e "c".
+    // e então armazena um valor em cada uma delas, sendo eles "X" ou "O"
     const [a, b, c] = lines[i];
+
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      //a repetição do squares[a] é para verificar se a primeira não é "null"
+      // ja as outra é de comparação, para verificar se todas as posições são do mesmo jogador, "X" ou "O"
+
+
       return squares[a];
+      //esse retorna quem ganhou
+      // dá pra colocar algo aqui como uma section com efeitos para o jogador
+
+
+
     }
   }
+
+
+
+
   return null;
+  // esse é se não houver nenhuma combinação, ou se o jogo deu velha, então retorna nulo para que continue o jogo, ou acabar se for velha
 }
+//---------------------------------------
